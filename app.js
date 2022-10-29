88915c09a0bc69cf858d470bb0f7ad6c3163751c
melodoc <liubov_rz@yahoo.com>
2022-10-30 00:31:55 +0400

    feat. Add errors and connection in the database #3

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-// eslint-disable-next-line no-console
-console.log('test');
+/* eslint-disable no-console */
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+const URI = 'mongodb://localhost:27017/bitfilmsdb';
+
+mongoose.connect(URI, {
+}, (err) => {
+  if (err) throw err.message;
+  console.log(`Connected to ${URI}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
