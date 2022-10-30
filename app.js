ffadb06582514d1fbd4b168a48a57813c4bb7b2c
melodoc <liubov_rz@yahoo.com>
2022-10-30 15:31:54 +0400

    feat. Add routes to entry point of app #4

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-/* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const auth = require('./middlewares/auth');
+const commonError = require('./middlewares/common-error');
+const userAuth = require('./routes/auth');
+const user = require('./routes/users');
+const movie = require('./routes/movies');
+const { HTTP_RESPONSE } = require('./constants/errors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const URI = 'mongodb://localhost:27017/bitfilmsdb';
+// to collect JSON format
+app.use(bodyParser.json());
+// for receiving web pages inside a POST request
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(URI, {
-}, (err) => {
-  if (err) throw err.message;
-  console.log(`Connected to ${URI}`);
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.use('/', userAuth);
+app.use(auth);
+app.use('/users', user);
+app.use('/movies', movie);
+app.all('/*', (req, res, next) => {
+  next(new Error(HTTP_RESPONSE.notFound.message));
 });
+
+app.use(errors());
+app.use(commonError);
+app.listen(PORT);
